4c00109c7d18665adf674e41ae8660e97fa13e6c
Ivo Ilić <3476570+ivoilic@users.noreply.github.com>
2026-04-16 02:20:53 -0400

    Removed the unused package `prettier-plugin-glsl`

--- packages/wasm-crustache/prettier.mjs
+++ packages/wasm-crustache/prettier.mjs
@@ -1,13 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-glsl'],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   printWidth: 100,
   singleQuote: true,
   arrowParens: 'always',
   importOrder: ['<THIRD_PARTY_MODULES>', '^~/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  overrides: [{ files: ['*.frag'], options: { parser: 'glsl-parser' } }],
 };
 
 export default config;
